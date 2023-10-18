9a1bc9603855a9e12f6520d9ac6ab828b061b0cd
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-10-18 21:05:50 +0700

    change header

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,14 +1,34 @@
 import { Button, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { NavigationProp } from "@react-navigation/native";
+import React, { useLayoutEffect } from "react";
+import {
+  NavigationProp,
+  ParamListBase,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
 import { FIREBASE_AUTH } from "../../FirebaseConfig";
 interface ListProps {
   navigation: NavigationProp<any, any>;
 }
+
+interface Params {
+  name?: string;
+}
 const List = ({ navigation }: ListProps) => {
+  const nav = useNavigation();
+  const route = useRoute();
+  const params = route.params as Params;
+  console.log(params);
+  useLayoutEffect(() => {
+    nav.setOptions({
+      headerTitle: "Change Header Title",
+    });
+  }, []);
   return (
     <View>
       <Text>List</Text>
+      <Text>{params.name}</Text>
       <Button
         title="Go to Details"
         onPress={() => {
