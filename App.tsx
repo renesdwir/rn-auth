a51b71a32214bada7fa4ce24afd9b1d609d917de
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-10-18 21:04:57 +0700

    add auth

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import Login from "./app/screens/Login";
 import List from "./app/screens/List";
 import Detail from "./app/screens/Detail";
 import { useEffect, useState } from "react";
 import { User, onAuthStateChanged } from "firebase/auth";
 import { FIREBASE_AUTH } from "./FirebaseConfig";
+import Navigation from "./Navigation";
 
 const Stack = createNativeStackNavigator();
 const InsideStack = createNativeStackNavigator();
@@ -22,19 +23,37 @@ function InsideLayout() {
 }
 export default function App() {
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      console.log("user", user);
-      setUser(user);
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (authUser) => {
+      console.log(authUser);
+      setUser(authUser);
+      setLoading(false);
     });
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         {user ? (
+          // <Stack.Screen
+          //   name="Inside"
+          //   component={InsideLayout}
+          //   options={{ headerShown: false }}
+          // />
           <Stack.Screen
             name="Inside"
-            component={InsideLayout}
+            component={Navigation}
             options={{ headerShown: false }}
           />
         ) : (
